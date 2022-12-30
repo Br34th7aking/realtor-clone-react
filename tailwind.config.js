40a4077c6c8720242dcd5bd299c216f7c44bf4ef
br34th7aking <abhijit.raj@mycol.in>
2022-12-30 23:15:38 +0530

    Add the header and navigation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js, jsx, tx, tsx}"],
+  content: ["./src/**/*.{js,jsx,tx,tsx}"],
   theme: {
     extend: {},
   },
