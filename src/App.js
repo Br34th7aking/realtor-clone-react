220a00f44a74f7ee8a368626eab9e84903a091a0
br34th7aking <abhijit.raj@mycol.in>
2022-12-30 21:12:40 +0530

    Create pages and add routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ForgotPassword from "./pages/ForgotPassword";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
 function App() {
   return (
     <>
-      <h1 className="text-2xl">Hello World!</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/offers" element={<Offers />} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./scr/**/*.{js, jsx, tx, tsx}"],
+  content: ["./src/**/*.{js, jsx, tx, tsx}"],
   theme: {
     extend: {},
   },
